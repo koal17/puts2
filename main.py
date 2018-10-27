b41f749022f1359d339aa5c83e7e46f9be601737
koal17 <alekhyakondapalli7@gmail.com>
2018-10-27 11:29:38 +0200

    the intopn are added

--- main.py
+++ main.py
@@ -7,12 +7,38 @@ def index():
     return 'Usage;\n<Operation>?A=<Value1>&B=<Value2>\n'
 
 
-@app.route('/add a')
+@app.route('/add')
 def addition():
     value1=request.args.get('A',default = 0, type = int)
     value2=request.args.get('B',default = 0, type = int)
     result=value1+value2
     return '%d \n' % result
+
+@app.route('/sub')
+def substraction():
+    value1=request.args.get('A',default = 0, type = int)
+    value2=request.args.get('B',default = 0, type = int)
+    result=value1-value2
+    return '%d \n' % result
+
+
+@app.route('/multi')
+def multiplication():
+    value1=request.args.get('A',default = 0, type = int)
+    value2=request.args.get('B',default = 0, type = int)
+    result=value1*value2
+    return '%d \n' % result
+
+
+
+@app.route('/divi')
+def division():
+    value1=request.args.get('A',default = 0, type = float)
+    value2=request.args.get('B',default = 0, type = float)
+    result=value1/value2
+    return '%2f \n' % result
+
+
 
 
 if __name__ == "__main__":
